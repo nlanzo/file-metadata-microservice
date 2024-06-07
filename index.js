8537c3e93eda96b3180372d1ac6a9eed74928e5a
Nick Lanzo <nlanzo@gmail.com>
2024-06-07 16:03:03 -0500

    Initial commit

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
+const fs = require('fs');
+const path = require('path');
 require('dotenv').config()
 
 var app = express();
@@ -12,7 +16,25 @@ app.get('/', function (req, res) {
 });
 
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  
+  // send response
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
 
+  // delete file after response
+  fs.readdir('./uploads', (err, files) => {
+    if (err) throw err;  
+    for (const file of files) {
+      fs.unlink(path.join('./uploads', file), (err) => {
+        if (err) throw err;
+      });
+    }
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
